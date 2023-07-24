bc5e7dadb781ca0944d87905b552227d7181fb72
Devansh Parapalli <parapallidev@gmail.com>
2023-07-25 00:30:02 +0530

    Tried to solve AI:P1, Theory for PS:P2

---
+++ AI/P1_WaterJugProblem/main.py
@@ -0,0 +1,86 @@
+from collections import deque
+
+
+def _gcd(a, b):
+    res = min(a, b)
+    while res > 0:
+        if a % res == 0 and b % res == 0:
+            break
+        res -= 1
+    return res
+
+
+def is_solvable(a, b, target):
+    if a + b < target:
+        return False
+    if (a == 0 and b == 0):
+        return target == 0
+    if (target % _gcd(a, b) == 0):
+        return True
+    return False
+
+def is_valid(a, b, current):
+    if a <= 0 or b <= 0 or a < current[0] or b < current[1]:
+        return False
+    return True
+
+def water_jug_bfs(a, b, target):
+    solution_exists = is_solvable(a, b, target)
+    if not solution_exists:
+        raise ValueError("No solution exists")
+    solution_found = False
+    mark: set[tuple[int, int]] = set()
+    queue: deque[tuple[int, int]] = deque()
+    path: list[tuple[int, int]] = list()
+    
+    queue.append((0, 0))
+    
+    while len(queue) > 0:
+        current = queue.popleft()
+        if current in mark:
+            # Already visited state
+            continue
+        if not is_valid(a, b, current):
+            # Invalid State
+            continue
+        
+        path.append(current)
+        
+        mark.add(current) # Mark the current state as visited.
+        
+        if current[0] == target or current[1] == target:
+            # Solution found
+            solution_found = True
+            if current[0] == target:
+                path.append((current[0], 0))
+            else:
+                path.append((0, current[1]))
+            break
+        
+        # Incase we didn't find solution
+        queue.append((a, current[1])) # R1: Fill the first jug 
+        queue.append((current[0], b)) # R2: Fill the second jug
+        
+        # R3: Pour from A to B
+        check_current = (current[0] - (b - current[1]), b) if current[0] > b - current[1] else (0, current[0] + current[1])
+        if is_valid(a, b, check_current):
+            queue.append(check_current)
+        
+        # R4: Pour from B to A
+        check_current = (a, current[1] - (a - current[0])) if current[1] > a - current[0] else (current[0] + current[1], 0)
+        if is_valid(a, b, check_current):
+            queue.append(check_current)
+        
+        queue.append((0, current[1])) # R5 : Empty first jug
+        queue.append((current[0], 0)) # R6 : Empty second jug
+        
+    return solution_exists, solution_found, path
+
+if __name__ == "__main__":
+    se, sf, p = water_jug_bfs(5, 3, 2)
+    if se == sf == True: # Check to make sure that errors don't compromise answer
+        _pl = len(p)
+        for pe, idx in enumerate(p):
+            print(f"{idx[0]} {idx[1]}", end="")
+            if pe != _pl - 1:
+                print(" -> ", end="")
---
+++ PS/P2_ComplexTable/README.md
@@ -0,0 +1,32 @@
+# P2_ComplexTable
+
+The `<table>` HTML element represents tabular data — that is, information presented in a two-dimensional table comprised of rows and columns of cells containing data.
+
+Attributes:
+
+- `align` - Specifies the alignment of a table according to surrounding text.
+- `bgcolor` - Specifies the background color for a table.
+- `border` - Specifies the width of the border around a table.
+- `cellpadding` - Specifies the space between the cell wall and the cell content.
+- `cellspacing` - Specifies the space between cells.
+- `frame` - Specifies which parts of the outside borders that should be visible.
+- `rules` - Specifies which parts of the inside borders that should be visible.
+- `summary` - Specifies a summary of the content of a table.
+- `width` - Specifies the width of a table.
+
+Note : All these attributes are deprecated in HTML5. It is advised to use CSS instead.
+
+`<table>` element has the following descendants:
+
+- `<thead> - <tbody> - <tfoot>` - Table Sections
+- `<captions>` - Table Caption
+- `<colgroup>` - Table Column Group (which contains `col`)
+
+`<thead> - <tbody> - <tfoot>` elements have the following descendants:
+
+- `<tr>` - Table Row
+
+`<tr>` element has the following descendants:
+
+- `<th>` - Table Header Cell
+- `<td>` - Table Data Cell
