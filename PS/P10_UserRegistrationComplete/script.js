bbde2fbec3a7ede9b8a6c1b68e76c838a78e78ab
Devansh Parapalli <parapallidev@gmail.com>
2023-08-21 23:04:12 +0530

    frame for P10

---
+++ PS/P10_UserRegistrationComplete/script.js
@@ -0,0 +1,9 @@
+
+const USERNAME = "DevParapalli"
+const PASSWORD = "shhh!iamasecret"
+
+// map to input fields
+let currentUserName = ""; 
+let currentPassword = "";
+
+// TODO: Create onLoginPageSubmit, onSignUpPageSubmit, onForgotPageSubmit, render and load functions.
